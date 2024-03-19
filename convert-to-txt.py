8c5615ce2fc9717489b9e5a49170a8bc64923dc7
suqik <suchen@shao.ac.cn>
2024-03-19 22:14:53 +0800

    support reading AHF halo

--- convert-to-txt.py
+++ convert-to-txt.py
@@ -2,17 +2,22 @@ import sys
 import numpy as np
 from nbodykit.source.catalog.file import BigFileCatalog
 
-file = sys.argv[1]
-ftype = sys.argv[2]
-ofile = sys.argv[3]
+fclass = sys.argv[1]
+file = sys.argv[2]
+ftype = sys.argv[3]
+ofile = sys.argv[4]
 
-if ftype.lower() == 'dm':
-    dataset = '1/'
-elif ftype.lower() == 'halo':
-    dataset = 'RFOF/'
-else:
-    print("Cannot recognize the dataset type. Only `dm` or `halo` (Case insensitive).")
-    exit(1)
+if fclass.lower() == 'bigfile':
+    if ftype.lower() == 'dm':
+        dataset = '1/'
+    elif ftype.lower() == 'halo':
+        dataset = 'RFOF/'
+    else:
+        print("Cannot recognize the dataset type. Only `dm` or `halo` (Case insensitive).")
+        exit(1)
 
-cat = BigFileCatalog(file, dataset=dataset, header='Header')
-np.savetxt(ofile, np.array(cat['Position']), fmt='%.3f %.3f %.3f')
+    cat = BigFileCatalog(file, dataset=dataset, header='Header')
+    np.savetxt(ofile, np.array(cat['Position']), fmt='%.3f %.3f %.3f')
+if fclass.lower() == 'ahf':
+    cat = np.loadtxt(file)
+    np.savetxt(ofile, cat[:,5:8], fmt='%.3f %.3f %.3f')
