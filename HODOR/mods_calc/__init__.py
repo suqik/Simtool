ecc246b9a739820e5ce3640bef4a1ce2596736af
suqik <suchen@shao.ac.cn>
2024-04-26 14:31:13 +0800

    rm HODOR

--- HODOR/mods_calc/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-
-from .estimate_chi2 import Chi2Class
-from .optimizers import ClassMinimizer
-from .compute_2pcf import ComputeCFClass
-from .compute_pspec import ComputePKClass
-from .estimate_ndens import DensClass
-
--- HODOR/mods_hod/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-
-
-from .apply_hod import ModelClass
-from .data import DataClass
