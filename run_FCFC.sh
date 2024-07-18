0acf8e867dcb7f05e9bf656e438ec288c19300e8
suqik <suchen@shao.ac.cn>
2024-07-18 15:56:14 +0800

    update

--- run_FCFC.sh
+++ run_FCFC.sh
@@ -7,7 +7,7 @@ WDIR=${HOME}/Program/SBI_Void/simulator/fastpm
 PYTHON=python
 MPIRUN=/usr/local/bin/mpirun
 MKCFG=${WDIR}/Simtool/make-cfgs.py
-FCFC=${HOME}/applications/Measurements/FCFC/FCFC_2PT
+FCFC=${HOME}/applications/Measurements/FCFC_intel/FCFC_2PT
 DDIR=${HOME}/Data/BOSS_dr12
 
 cfgpath=${WDIR}/cfgs
@@ -16,14 +16,13 @@ if [ ! -d $cfgpath ]; then
 fi
 
 ### FCFC CONFIGURATION SETUP
-export OMP_NUM_THREADS=1
 
 fcfc_type=fcfc_sky
 
 binning=2 # 0 for 2pcf, 1 for multipoles, 2 for proj_cf
-smin=20
-smax=200
-size=36
+smin=0
+smax=75
+size=3
 
 if [ $binning -eq 1 ]; then
 	mu="[0,2,4]"
@@ -35,64 +34,51 @@ fi
 
 if [ $binning -eq 2 ]; then
 	pj=T
-	pimin=20
-	pimax=200
+	pimin=0
+	pimax=75
 else
 	pj=F
 	pimin=None
 	pimax=None
 fi
 
-for ((i=1; i<2; i++)) do
-	# DM input file
-    if [ $i -lt 10 ];
-	then
-		idx=000${i}
-	elif [ $i -lt 100 ];
-	then
-		idx=00${i}
-	elif [ $i -lt 1000 ];
-	then 
-		idx=0${i}
-	elif [ $i -lt 10000 ];
-	then
-		idx=${i}
-	fi
-    dfile=${DDIR}/mocks/mock_galaxy_DR12_LOWZ_N_QPM_${idx}.rdzw 
-    # Void input file
-    rfile=${DDIR}/mocks/mock_random_DR12_LOWZ_N_50x1.rdzw
-	if [ ! -d ${WDIR}/multipoles/ ]; then
-		mkdir -p ${WDIR}/multipoles
-	fi
+basename=DR12v5_LOWZ_North
 
-	opfilebase=${WDIR}/multipoles/mock_LOWZ_N_QPM_${idx}
-	cffile=${opfilebase}.clu
-	if [ $binning -eq 1 ]; then
-		mufile=${opfilebase}.multi
-	else
-		mufile=None
-	fi
-	if [ $binning -eq 2 ]; then
-		pjfile=${opfilebase}.proj
-	else
-		pjfile=None
-	fi
-	cfgfile=$cfgpath/fcfc_${idx}.conf
+dfile=${DDIR}/galaxy_${basename}.fits
+rfile=${DDIR}/random0_${basename}.fits
 
-	$PYTHON $MKCFG $fcfc_type \
-	-CATALOG "[$dfile, $rfile]" -CATALOG_LABEL "[D, R]" \
-	-ASCII_FORMATTER "[\"%f %f %f %f %f\", \"%f %f %f %f\"]" -POSITION "[\$1, \$2, \$3, \$1, \$2, \$3]" -WEIGHT "[\$4 * \$5, \$4]" \
-	-OMEGA_M 0.31 -OMEGA_LAMBDA 0.69 -DE_EOS_W m1 -CMVDST_ERR "1e-10" \
-	-BINNING_SCHEME $binning -PAIR_COUNT "[DD, DR, RR]" -PAIR_COUNT_FILE "[pairs.dd, pairs.dr, pairs.rr]" -CF_ESTIMATOR "(DD - 2*DR + RR)/RR" -CF_OUTPUT_FILE "$cffile" \
-	-SEP_BIN_MIN $smin -SEP_BIN_MAX $smax -SEP_BIN_SIZE $size \
-	-MULTIPOLE $mu -MULTIPOLE_FILE $mufile -MU_BIN_NUM $mubin \
-	-PROJECTED_CF $pj -PROJECTED_FILE $pjfile -PI_BIN_MIN $pimin -PI_BIN_MAX $pimax -PI_BIN_SIZE $size \
-	-ofile $cfgfile
+resultdir=${WDIR}/results
+if [ ! -d ${resultdir}/ ]; then
+	mkdir -p ${resultdir}
+fi
 
-	# $MPIRUN -np 16 $FCFC -c $cfgfile
-	# rm pairs.dd pairs.dr 
+opfilebase=${resultdir}/galaxy_${basename}
+cffile=${opfilebase}.clu
+if [ $binning -eq 1 ]; then
+	mufile=${opfilebase}.multi
+else
+	mufile=None
+fi
+if [ $binning -eq 2 ]; then
+	pjfile=${opfilebase}.proj
+else
+	pjfile=None
+fi
+cfgfile=$cfgpath/fcfc_data.conf
 
-done
+$PYTHON $MKCFG $fcfc_type \
+-CATALOG "[$dfile, $rfile]" -CATALOG_TYPE "[1, 1]" -CATALOG_LABEL "[D, R]" \
+-POSITION "[\${RA}, \${DEC}, \${Z}, \${RA}, \${DEC}, \${Z}]" -WEIGHT "[\${WEIGHT_SYSTOT} * \${WEIGHT_NOZ} * \${WEIGHT_CP} * \${WEIGHT_FKP}, \${WEIGHT_FKP}]" -SELECTION "[\${Z}<0.43 && \${Z}>0.2, \${Z}<0.43 && \${Z}>0.2]" \
+-OMEGA_M 0.31 -OMEGA_LAMBDA 0.69 -DE_EOS_W m1 -CMVDST_ERR "1e-10" \
+-BINNING_SCHEME $binning -PAIR_COUNT "[DD, DR, RR]" -PAIR_COUNT_FILE "[${opfilebase}.dd, ${opfilebase}.dr, ${opfilebase}.rr]" -CF_ESTIMATOR "(DD - 2*DR + RR)/RR" -CF_OUTPUT_FILE "$cffile" \
+-SEP_BIN_MIN $smin -SEP_BIN_MAX $smax -SEP_BIN_SIZE $size \
+-MULTIPOLE $mu -MULTIPOLE_FILE $mufile -MU_BIN_NUM $mubin \
+-PROJECTED_CF $pj -PROJECTED_FILE $pjfile -PI_BIN_MIN $pimin -PI_BIN_MAX $pimax -PI_BIN_SIZE $size \
+-ofile $cfgfile
+
+$FCFC -c $cfgfile
+# rm pairs.dd pairs.dr 
+
 ### end of the job
 
 end=`date +%s`
